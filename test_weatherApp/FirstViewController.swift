6f269b37dcc9bcfe55d86616dda15daf1ca75019
JunYoungJee <rainmaker8492@gmail.com>
2022-09-06 15:35:56 +0900

    프로젝트 생성 + 스토리보드 없이 앱 구동되도록 처리

---
+++ test_weatherApp/FirstViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  test_weatherApp
+//
+//  Created by sokol on 2022/09/06.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func loadView() {
+        self.view = UIView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ test_weatherApp/SecondViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SecondViewController.swift
+//  test_weatherApp
+//
+//  Created by sokol on 2022/09/06.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func loadView() {
+        self.view = UIView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
