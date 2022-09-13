f97007ec39d7b324c792d814dbca2599acf7271b
JunYoungJee <rainmaker8492@gmail.com>
2022-09-13 22:42:48 +0900

    Revert "Revert "1번뷰컨 - 검색 후 검색결과에 따라 api 다시 부르는 기능 추가""
    This reverts commit 8816ea282d48fbfec7ec6d2d2095d7b86ee0292f.

--- test_weatherApp/BasicModel/BasicSearchViewModel.swift
+++ test_weatherApp/BasicModel/BasicSearchViewModel.swift
@@ -10,4 +10,26 @@ import Foundation
 //FirstVC에서 도시검색 용 텍스트필드 들어갈 뷰에 쓰일 뷰모델
 class BasicSearchViewModel {
     
+    //input
+    var enterKeyPressed: (String) -> () = { userInput in }
+    
+    //output
+    var propergateUserInput: (String) -> () = { userInput in }
+    
+    //properties
+    init() {
+        bind()
+    }
+    
+    private func bind() {
+        enterKeyPressed = { [weak self] userInput in
+            guard let filteredInput = self?.removeSpaces(userInput: userInput) else { return }
+            self?.propergateUserInput(filteredInput)
+        }
+    }
+    
+    private func removeSpaces(userInput: String) -> String {
+        return userInput.replacingOccurrences(of: " ", with: "")
+    }
+    
 }
